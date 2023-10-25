6263e44667966ea0c67d83d6a6ff5643605f7e54
zcomert <zcomert@samsun.edu.tr>
2023-10-25 11:59:12 +0300

    YoutubeApp has been done.

--- YoutubeApp/Controllers/VideosController.cs
+++ YoutubeApp/Controllers/VideosController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using YoutubeApp.Data;
+using YoutubeApp.Models;
 
 namespace YoutubeApp.Controllers;
 
@@ -6,5 +8,42 @@ namespace YoutubeApp.Controllers;
 [Route("api/videos")]
 public class VideosController : ControllerBase
 {
-    
+    private readonly VideoRepository _videoRepository;
+
+    public VideosController(VideoRepository videoRepository)
+    {
+        _videoRepository = videoRepository;
+    }
+
+    [HttpGet] // localhost/api/videos
+    public List<Video> GetAllVideos()
+    {
+        return _videoRepository.GetAllVideo();
+    }
+
+    [HttpGet("{id:int}")] // localhost/api/videos/:id
+    public Video GetOneVideo(int id)
+    {
+        return _videoRepository.GetOneVideo(id);
+    }
+
+    [HttpPut("{id:int}")] // localhost/api/videos/:id
+    public void UpdateOneVideo(int id, Video video)
+    {
+        _videoRepository.UpdateOneVideo(id, video);
+    }
+
+    [HttpPost] // localhost/api/videos
+    public void CreateOneVideo(Video video)
+    {
+        _videoRepository.CreateOneVideo(video);
+    }
+
+    [HttpDelete("{id:int}")] // localhost/api/videos/:id
+    public void DeleteOneVideo(int id)
+    {
+        _videoRepository.Delete(id);
+    }
+
+
 }
--- YoutubeApp/Program.cs
+++ YoutubeApp/Program.cs
@@ -1,3 +1,5 @@
+using YoutubeApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<VideoRepository>();
 
 var app = builder.Build();
 
